a23f127c71d7c21d9eba0d53aa22d90b665bc519
Flo2410 <florian@hye.dev>
2023-01-10 10:57:50 +0100

    added vars to upload.sh

--- upload.sh
+++ upload.sh
@@ -4,11 +4,18 @@
 #scp -r ./**/ mobi_theta:/home/mobi/catkin_ws/src
 # ssh mobi_theta "cd /home/mobi/catkin_ws/ && source /home/mobi/catkin_ws/devel/setup.bash && catkin_make"
 
+#host = ros
+#remote_path = "/home/florian/catkin_ws"
+
+host=mobi_theta
+remote_path="/home/mobi/catkin_ws"
 
 
-ssh ros "screen -X -S pozyx-fake kill"
-ssh ros "screen -X -S web-backend kill"
-scp -r ./**/ ros:/home/florian/catkin_ws/src
-ssh ros "cd /home/florian/catkin_ws/ && source /home/florian/catkin_ws/devel/setup.bash && catkin_make"
-ssh ros "source /home/florian/catkin_ws/devel/setup.bash && screen -S pozyx-fake -d -m rosrun pozyx-fake pozyx-fake.py"
-ssh ros "source /home/florian/catkin_ws/devel/setup.bash && screen -S web-backend -d -m rosrun web-backend web-backend.py"
+ssh $host "screen -X -S pozyx-translator kill"
+# ssh $host "screen -X -S pozyx-fake kill"
+ssh $host "screen -X -S web-backend kill"
+scp -r ./**/ $host:$remote_path/src
+ssh $host "cd $remote_path/ && source $remote_path/devel/setup.bash && catkin_make"
+#ssh $host "source $remote_path/devel/setup.bash && screen -S pozyx-fake -d -m rosrun pozyx-fake pozyx-fake.py"
+ssh $host "source $remote_path/devel/setup.bash && screen -S pozyx-translator -d -m rosrun pozyx-translator pozyx-mqtt.py"
+ssh $host "source $remote_path/devel/setup.bash && screen -S web-backend -d -m rosrun web-backend web-backend.py"
